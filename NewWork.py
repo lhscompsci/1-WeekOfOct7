621737f8740d224c596e44f4d0f57398098fff01
Black, Samuel Thomas <sam.black@gatech.edu>
2019-10-09 08:31:05 -0500

    pushing 1st version of 10-9's work

--- NewWork.py
+++ NewWork.py
@@ -50,3 +50,10 @@ print(formatter % (formatter, formatter, formatter, formatter))
 
 # Why did I use %r instead of %s?
 
+# Time for some strange stuff in the world of printing...
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("Here are the days: ", days)
+print("Here are the months: ", months)
